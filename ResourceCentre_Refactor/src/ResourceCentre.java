aacaf5e3696aa074e54565f501019fcb82205d22
Danis <Danis@Danish>
2022-07-25 22:23:53 +0800

    Update ResourceCentre retrieveAllChromebook

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -168,7 +168,7 @@ public class ResourceCentre {
 
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
-		// write your code here
+		
 		for (int i = 0; i < chromebookList.size(); i++) {
 
 			output += String.format("%-84s \n", chromebookList.get(i).toString());
